f0341f3a670238d0afeadda0dfd60bb693df467c
Louis Nderitu <louisnderitu20@gmail.com>
2025-10-25 23:27:53 +0300

    Email issues

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -23,7 +23,11 @@ export async function POST(req: NextRequest) {
 
           <!-- HEADER -->
           <div style="background: linear-gradient(90deg, #004aad, #009ffd); padding: 25px; text-align: center;">
-            <img src=""https://geoinsights.vercel.app/logo.png" alt="GeoInsights Logo" style="max-width: 120px; margin-bottom: 10px;" />
+            <img src="https://geoinsights.vercel.app/logo.png" 
+              alt="GeoInsights Logo" 
+              style="max-width: 120px; margin-bottom: 10px; display: block; border: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic;" 
+              width="120" 
+              height="auto" />
             <h1 style="color: #ffffff; font-size: 22px; margin: 0;">New Message from GeoInsights</h1>
           </div>
 
